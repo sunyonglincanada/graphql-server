b9186ef8436ccd088e59eaa1747c5973a4ca5da3
Eric Sun <sunyonglincanada@gmail.com>
2020-05-30 22:17:39 -0400

    Create RootQuery with hardcoded data.

--- schema.js
+++ schema.js
@@ -7,4 +7,44 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-module.exports = new GraphQLSchema({});
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+  name: 'Customer',
+  fields: () => ({
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  })
+});
+
+// Hardcoded Data
+const customers = [
+  { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
+  { id: '2', name: 'Jim Green', email: 'jgreen@gmail.com', age: 27 },
+  { id: '3', name: 'Li Lei', email: 'llei@gmail.com', age: 15 }
+];
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    customer: {
+      type: CustomerType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        for (let i = 0; i < customers.length; i++) {
+          if (customers[i].id === args.id) {
+            return customers[i];
+          }
+        }
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
