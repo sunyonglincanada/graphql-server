eb1159454e4a43657ad5b314ef01f201af7fabe3
Eric Sun <sunyonglincanada@gmail.com>
2020-05-30 22:37:21 -0400

    Setup json server and fetch data using the json server.

--- schema.js
+++ schema.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -18,12 +19,14 @@ const CustomerType = new GraphQLObjectType({
   })
 });
 
+/*
 // Hardcoded Data
 const customers = [
   { id: '1', name: 'John Doe', email: 'jdoe@gmail.com', age: 35 },
   { id: '2', name: 'Jim Green', email: 'jgreen@gmail.com', age: 27 },
   { id: '3', name: 'Li Lei', email: 'llei@gmail.com', age: 15 }
 ];
+*/
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
@@ -35,17 +38,24 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
+        /*
         for (let i = 0; i < customers.length; i++) {
           if (customers[i].id === args.id) {
             return customers[i];
           }
         }
+        */
+        return axios
+          .get(`http://localhost:3000/customers/${args.id}`)
+          .then((res) => res.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        return axios
+          .get('http://localhost:3000/customers/')
+          .then((res) => res.data);
       }
     }
   }
